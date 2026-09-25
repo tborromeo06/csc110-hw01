65fabc5ab6b51a258e8b33e0f2d9b3a96fe202f2
Thalia Borromeo <tborromeo@smith.edu>
2026-09-25 12:00:20 -0400

    complete

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: Thalia Borromeo
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,6 +45,21 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+    x = 27
+    y = 1
+    a = 1.5
+    b = 7
+    c = -1
+    
+    result1=(3*x-9*y) / (2*a*(b-c))
+    
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result1)
+
 
     # End of Part 1 ----------------------
 
@@ -52,6 +67,14 @@ def main():
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
+    x = 5
+    y = -3
+    
+    result2=(x**2) * (y**4)
+    
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result2)
 
     # End of Part 2 ----------------------
 
@@ -60,6 +83,16 @@ def main():
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
+    a = 100
+    b = 13
+    
+    result3 = int(a/b)
+    
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", result3)
+
+
 
     # End of Part 3 ----------------------
 
@@ -67,6 +100,10 @@ def main():
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
+    result4 = a%b
+    
+    print("Part 4: result =", result4)
+
 
     # End of Part 4 ----------------------
 
